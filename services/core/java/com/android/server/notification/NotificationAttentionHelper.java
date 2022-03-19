e130d156ac69257b1f1604f39da8b8fb473d986d
June <june@eridan.me>
2022-03-19 17:10:28 +0200

    Support forwarding notifications from other users
    Co-authored-by: inthewaves <inthewaves@pm.me>
    Co-authored-by: June <june@eridan.me>
    Co-authored-by: quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
    Co-authored-by: Pratyush <codelab@pratyush.dev>
    Change-Id: I90970cc47b1e037e26a29cc393f563c6c7280019

--- services/core/java/com/android/server/notification/NotificationAttentionHelper.java
+++ services/core/java/com/android/server/notification/NotificationAttentionHelper.java
@@ -1083,7 +1083,7 @@ public final class NotificationAttentionHelper {
         return true;
     }
 
-    private String disableNotificationEffects(NotificationRecord record, int listenerHints) {
+    String disableNotificationEffects(NotificationRecord record, int listenerHints) {
         if (mDisableNotificationEffects) {
             return "booleanState";
         }
