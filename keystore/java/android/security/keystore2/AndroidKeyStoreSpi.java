yaap/frameworks_base

base: Apply the SafetyNet workaround to PlayStore aswell
Play Store is used for the new Play Integrity API, extend the hack
to it aswell

Test: Device Integrity and Basic Integrity passes.

idoybh: Rewrote to fit how we currently apply the workaround

Signed-off-by: Dyneteve <dyneteve@hentaios.com>
Change-Id: Id607cdff0b902f285a6c1b769c0a4ee4202842b1

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -42,6 +42,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.yaap.PixelPropsUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -164,6 +165,9 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        if (PixelPropsUtils.getIsFinsky()) {
+            throw new UnsupportedOperationException("Blocking safetynet attestation for finsky");
+        }
         for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
             if (ste.getClassName().contains("DroidGuard")) {
                 throw new UnsupportedOperationException("Blocking safetynet attestation");

Tomoms/android_frameworks_base

keystore/java/android/security/keystore2/AndroidKeyStoreSpi.java
@@ -45,6 +45,7 @@ import android.system.keystore2.ResponseCode;
 import android.util.Log;
 
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.util.custom.PixelPropsUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -174,6 +175,9 @@ public class AndroidKeyStoreSpi extends KeyStoreSpi {
 
     @Override
     public Certificate[] engineGetCertificateChain(String alias) {
+        if (PixelPropsUtils.getIsFinsky()) {
+            throw new UnsupportedOperationException("Blocking safetynet attestation for finsky");
+        }
         for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
             if (ste.getClassName().contains("DroidGuard")) {
                 throw new UnsupportedOperationException("Blocking safetynet attestation");