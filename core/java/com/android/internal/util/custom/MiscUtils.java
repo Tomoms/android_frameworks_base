3f262b195aca93727d7709acc0d9cb3d95d7718c
Tommaso Fonda <fondatommaso2@gmail.com>
2023-12-18 17:37:11 +0100

    custom: Add MiscUtils class
    All credits to Evolution-X devs.
    
    Change-Id: I073f564e5c3482cb6268be3dbe8f2c2b7f3eee4d

---
+++ core/java/com/android/internal/util/custom/MiscUtils.java
@@ -0,0 +1,28 @@
+package com.android.internal.util.custom;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class MiscUtils {
+
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
+    }
+
+}
