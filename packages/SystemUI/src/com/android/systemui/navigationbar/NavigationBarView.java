5a0025e826abbefa3439d2e43c38cecef4744694
Michael Bestas <mkbestas@lineageos.org>
2024-07-03 07:05:52 +0300

    Merge tag 'android-14.0.0_r53' into staging/lineage-21.0_merge-android-14.0.0_r53
    Android 14.0.0 release 53
    
    # -----BEGIN PGP SIGNATURE-----
    #
    # iF0EABECAB0WIQRDQNE1cO+UXoOBCWTorT+BmrEOeAUCZoRX5AAKCRDorT+BmrEO
    # eNztAJwL49zXuIdAUa6fEb0OhVDR1cYB4ACeOVBE7F4xTYdZIRH3Gn5DeFwAkLQ=
    # =jkFb
    # -----END PGP SIGNATURE-----
    # gpg: Signature made Tue Jul  2 22:41:24 2024 EEST
    # gpg:                using DSA key 4340D13570EF945E83810964E8AD3F819AB10E78
    # gpg: Good signature from "The Android Open Source Project <initial-contribution@android.com>" [marginal]
    # gpg: initial-contribution@android.com: Verified 2554 signatures in the past
    #      2 years.  Encrypted 4 messages in the past 2 years.
    # gpg: WARNING: This key is not certified with sufficiently trusted signatures!
    # gpg:          It is not certain that the signature belongs to the owner.
    # Primary key fingerprint: 4340 D135 70EF 945E 8381  0964 E8AD 3F81 9AB1 0E78
    
    # By Ivan Chiang (2) and others
    # Via Android Build Coastguard Worker
    * tag 'android-14.0.0_r53':
      Fix HardwareRenderers becoming invalidated by TRIM
      Ensure that EdgeBackGestureHandler receives all configuration updates
      Don't prevent group volume adjustment in SysUI
      Don't cancel PendingIntents unless the stay_stopped flag is on.
      Fix update failure if font family was installed and upgraded to 14
      Allow to collect moving back task in open transition
      Prevent TextView refocus on escape key press
      Fix security vulnerability allowing apps to start from background
      [PM] Send ACTION_PACKAGE_CHANGED when the app is installed
      [PM] Send ACTION_PACKAGE_CHANGED when mimeGroups are changed
      Verify UID of incoming Zygote connections.
    
     Conflicts:
    	packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarControllerImpl.java
    
    Change-Id: I032368d140bb3adea5530189f0999c369da966ad

--- packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/NavigationBarView.java
@@ -1098,7 +1098,6 @@ public class NavigationBarView extends FrameLayout {
         updateIcons(mTmpLastConfiguration);
         updateRecentsIcon();
         updateCurrentRotation();
-        mEdgeBackGestureHandler.onConfigurationChanged(mConfiguration);
         if (uiCarModeChanged || mTmpLastConfiguration.densityDpi != mConfiguration.densityDpi
                 || mTmpLastConfiguration.getLayoutDirection() != mConfiguration.getLayoutDirection()) {
             // If car mode or density changes, we need to reset the icons.
