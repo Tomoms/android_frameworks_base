e26810b194946adf29a17d0dd73568b7f5020d8f
John Galt <johngaltfirstrun@gmail.com>
2024-07-09 19:38:13 -0400

    Revert "run full explicit GC in SystemUI and system_server after locking"
    This reverts commit 7abb92d9d954a8043b5305dc3e6f4ee2ca5dd64c.

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2819,9 +2819,6 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             userActivity();
             mUpdateMonitor.setKeyguardGoingAway(false);
             mKeyguardViewControllerLazy.get().setKeyguardGoingAwayState(false);
-            System.gc();
-            System.runFinalization();
-            System.gc();
             mShowKeyguardWakeLock.release();
         }
         mKeyguardDisplayManager.show();
