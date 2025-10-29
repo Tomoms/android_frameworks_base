1ea293e68a69d3063155b40a304633fa87dc0c70
Tommaso Fonda <fondatommaso2@gmail.com>
2025-10-29 15:47:19 +0100

    Revert "constify JNINativeMethod tables"
    This reverts commit fae8e0013427cbdc3c76e1224d1d4b7b6a704803.

--- ravenwood/runtime-jni/ravenwood_os_constants.cpp
+++ ravenwood/runtime-jni/ravenwood_os_constants.cpp
@@ -756,7 +756,7 @@ static void OsConstants_initConstants(JNIEnv* env, jclass) {
     initConstant(env, c, "_SC_XOPEN_XCU_VERSION", _SC_XOPEN_XCU_VERSION);
 }
 
-static const JNINativeMethod gMethods[] = {
+static JNINativeMethod gMethods[] = {
     NATIVE_METHOD(OsConstants, initConstants, "()V"),
 };
 
