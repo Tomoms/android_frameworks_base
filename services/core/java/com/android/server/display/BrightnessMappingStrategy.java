ec9596b9c5470b2362d0cb464e386e37e57b2835
Tommaso Fonda <fondatommaso2@gmail.com>
2026-04-16 14:58:54 +0200

    Revert "display: Don't reset brightness adjustment on clearUserDataPoints()"
    This reverts commit e831fffb5b4e15beb4a69114e06be14b729ce39e.

--- services/core/java/com/android/server/display/BrightnessMappingStrategy.java
+++ services/core/java/com/android/server/display/BrightnessMappingStrategy.java
@@ -643,6 +643,7 @@ public abstract class BrightnessMappingStrategy {
             }
 
             mMaxGamma = maxGamma;
+            mAutoBrightnessAdjustment = 0;
             mUserLux = INVALID_LUX;
             mUserBrightness = PowerManager.BRIGHTNESS_INVALID_FLOAT;
             if (mLoggingEnabled) {
@@ -746,6 +747,7 @@ public abstract class BrightnessMappingStrategy {
                     PLOG.start("clear user data points")
                             .logPoint("user data point", mUserLux, mUserBrightness);
                 }
+                mAutoBrightnessAdjustment = 0;
                 mUserLux = INVALID_LUX;
                 mUserBrightness = PowerManager.BRIGHTNESS_INVALID_FLOAT;
                 computeSpline();
@@ -1040,6 +1042,7 @@ public abstract class BrightnessMappingStrategy {
                     PLOG.start("clear user data points")
                             .logPoint("user data point", mUserLux, mUserBrightness);
                 }
+                mAutoBrightnessAdjustment = 0;
                 mUserLux = INVALID_LUX;
                 mUserBrightness = PowerManager.BRIGHTNESS_INVALID_FLOAT;
                 computeSpline();
