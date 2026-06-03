arpokrat/arpokrat_frameworks_base

Enforce cross profile clipboard access settings

services/core/java/com/android/server/wm/DragState.java
@@ -500,6 +500,10 @@ class DragState {
         final UserManagerInternal userManager = LocalServices.getService(UserManagerInternal.class);
         mCrossProfileCopyAllowed = !userManager.getUserRestriction(
                 mSourceUserId, UserManager.DISALLOW_CROSS_PROFILE_COPY_PASTE);
+        if (!android.ext.settings.CrossProfileClipboardAccessSettings
+                .isExportAccessAllowed(mDisplayContent.mAtmService.mContext, mSourceUserId)) {
+            mCrossProfileCopyAllowed = false;
+        }
 
         if (DEBUG_DRAG) {
             Slog.d(TAG_WM, "Broadcasting DRAG_STARTED at (" + touchX + ", " + touchY + ")");

Tomoms/android_frameworks_base

services/core/java/com/android/server/wm/DragState.java
@@ -514,6 +514,10 @@ class DragState {
         final UserManagerInternal userManager = LocalServices.getService(UserManagerInternal.class);
         mCrossProfileCopyAllowed = !userManager.getUserRestriction(
                 mSourceUserId, UserManager.DISALLOW_CROSS_PROFILE_COPY_PASTE);
+        if (!android.ext.settings.CrossProfileClipboardAccessSettings
+                .isExportAccessAllowed(mCurrentDisplayContent.mAtmService.mContext, mSourceUserId)) {
+            mCrossProfileCopyAllowed = false;
+        }
 
         if (DEBUG_DRAG) {
             Slog.d(TAG_WM, "Broadcasting DRAG_STARTED at (" + touchX + ", " + touchY + ")");